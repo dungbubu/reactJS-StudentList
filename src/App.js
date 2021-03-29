2d84fbe248527788e199b98c94568ad704497b4e
dungbubu <nguyenmanhdung1996@gmail.com>
2021-03-29 22:17:50 +0700

    29/3/21

--- src/App.js
+++ src/App.js
@@ -5,11 +5,11 @@ import "./App.css";
 const STUDENTLIST = 
   [
     {
-      id: 1,
-      name: 'Nguyen Manh Dung A',
+      id: 3,
+      name: 'Nguyen Manh Dung C',
       dob: '24/06/1996',
-      email: 'nguyenmanhdung1996A@gmail.com',
-      phone: '01234567891'
+      email: 'nguyenmanhdung1996C@gmail.com',
+      phone: '01234567893'
     },
 
     {
@@ -21,36 +21,100 @@ const STUDENTLIST =
     },
 
     {
-      id: 3,
-      name: 'Nguyen Manh Dung C',
+      id: 1,
+      name: 'Nguyen Manh Dung A',
       dob: '24/06/1996',
-      email: 'nguyenmanhdungC1996@gmail.com',
-      phone: '01234567893'
+      email: 'nguyenmanhdung1996A@gmail.com',
+      phone: '01234567891'
     },
   ]
 
 function App() {
-  // Khai bao bien State
+  // Khai báo biến State
   const [studentList, setStudentList] = useState(STUDENTLIST)
-  const [studentInfo, setStudentInfo] = useState(0) // Bien nay de luu thong tin (ID)
-  const [modals, setModals] = useState(false) // Bien nay de an/hien Modal
+  const [studentInfo, setStudentInfo] = useState(0) // Biến này để lưu thông tin (ID) khi xóa
+  const [modals, setModals] = useState(false) // Biến này để ẩn/hiện Modal
+  const [inputTable, setInputTable] = useState(false) // Biến này để ẩn/hiện bảng thêm mới học viên
+  const [addNew, setAddNew] = useState([]) // Biến này để lưu thông tin học viên mới
 
-  // Ham goi ra Modal xuat hien
+  const [newStudentId, setNewStudentId] = useState(0); // Các biến thông tin của học viên mới
+  const [inputName, setInputName] = useState();
+  const [inputDob, setInputDob] = useState();
+  const [inputEmail, setInputEmail] = useState();
+  const [inputPhone, setInputPhone] = useState();
+
+
+
+  // Hàm gọi Modal xuất hiện
   function showModal(studentID)
     {
       setStudentInfo(studentID.id)
       setModals(true)
     }
-
-  function cancelDelete()
+  // Nút chức năng xác nhận xóa/không xóa
+  function cancelDelete() // Đây là nút cancel
     {
       setModals(false)
     }
-  function realDelete(studentID) //Đây là nút Ok
+  function realDelete(studentID) // Đây là nút Ok
     {
       setStudentList(studentList.filter(student => student.id !== studentInfo ));
       setModals(false)
     }
+
+  // Hàm gọi bảng thêm mới học viên
+  function showInputTable()
+    {
+      setInputTable(true)
+    }
+  // Nút chức năng xác nhận thêm mới học viên
+  function cancelAdd() // Đây là nút cancel
+    {
+      setInputTable(false)
+    }
+  function addData() // Đây là nút Ok
+    {
+      let newId = studentList[0].id + 1;
+      setNewStudentId(newId)
+      console.log("ID của học viên mới là: " + newId)
+
+      let newStudent = 
+        {
+          id: newId,
+          name : inputName,
+          dob : inputDob,
+          email : inputEmail,
+          phone : inputPhone
+        }
+      
+      studentList.unshift(newStudent);
+      setInputTable(false)
+    }
+
+  // Lưu thông tin học viên mới vào biến state
+  function updateName(event)
+    {
+      const inputNameValue = event.target.value;
+      setInputName(inputNameValue);
+    }
+  function updateDob(event)
+    {
+      const inputDobValue= event.target.value;
+      setInputDob(inputDobValue);
+    }
+  function updateEmail(event)
+    {
+      const inputEmailValue= event.target.value;
+      setInputEmail(inputEmailValue);
+    }
+  function updatePhone(event)
+    {
+      const inputPhoneValue= event.target.value;
+      setInputPhone(inputPhoneValue);
+    }
+
+
+
 
   // Render
   const listItems = studentList.map((student) =>
@@ -64,28 +128,56 @@ function App() {
       </tr>
     ));
 
+
+
+  
+  // Phần hiển thị Html
   return (
     <div className="studentList">
         <header className="container">
-        <h1 class="title">Danh sách học viên</h1>
-        <button id="add-student">Thêm học viên</button>
+          <div className="dashboard">
+            <h1>Danh sách học viên</h1>
+            <button className="btn btn-add" onClick={showInputTable}>Thêm học viên</button>
+          </div>
         </header>
           
           <main className="container">
-            {/* Modal xac nhan co xoa nguoi nay hay khong */}
-            {modals === true && 
-              <div>
-                <div className="delete-form-text"><p>Bạn chắc chắn sẽ xóa người này chứ</p></div>
-          
-                <div className="delete-group-btn">
-                  <button className="confirm-btn" onClick={realDelete}>Yes</button>
-                  <button className="cancel-btn" onClick={cancelDelete}>Cancel</button>
-                </div>
-              </div>}
+            {/* Bảng điền thông tin học viên */}
+            {inputTable === true && 
+              <div className="dashboard">
+                <h2>Thêm mới học viên</h2>
 
-            <table className="table table table-striped table-hover">
-              {/* Hang tieu de cua bang */}
-              <thead className="thead-dark">
+                <p>Họ tên:</p>
+                <input type="text" placeholder="..." defaultValue="" onChange={(event) => updateName(event)}></input>
+          
+                <p>Năm sinh:</p>
+                <input type="text" placeholder="..." defaultValue="" onChange={(event) => updateDob(event)}></input>
+            
+                <p>Email:</p>
+                <input type="text" placeholder="..." defaultValue="" onChange={(event) => updateEmail(event)}></input>
+            
+                <p>Điện thoại:</p>
+                <input type="text" placeholder="..." defaultValue="" onChange={(event) => updatePhone(event)}></input>
+                
+                <button className="btn btn-yes" onClick={addData}>Lưu</button>
+                <button className="btn btn-no"  onClick={cancelAdd}>Quay lại</button>
+              </div>
+            }
+
+            {/* Modal xác nhận xóa */}
+            {modals === true && 
+              <div className="dashboard">
+                <h2>Bạn chắc chắn sẽ xóa người này chứ</h2>
+                <div className="text-center">
+                  <button className="btn btn-yes" onClick={realDelete}>Yes</button>
+                  <button className="btn btn-no" onClick={cancelDelete}>Cancel</button>
+                </div>
+              </div>
+            }
+
+            <table className="table dashboard">
+              {/* Bảng danh sách học viên (tiêu đề) */}
+              <thead>
                 <tr>
                   <th scope="col">Họ tên</th>
                   <th scope="col" className="text-right">Năm sinh</th>
@@ -95,11 +187,10 @@ function App() {
                 </tr>
               </thead>
 
-              {/* Hang noi dung cua bang */}
-              <tbody id="data-table">
+              {/* Bảng danh sách học viên (nội dung) */}
+              <tbody>
                 {listItems}
               </tbody>
-
             </table>
           </main>
       </div>
